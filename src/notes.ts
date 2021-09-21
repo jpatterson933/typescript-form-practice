3460879254ded068a4e079a7e36fb8fc98e39614
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-21 10:45:29 -0700

    reviewing and adding more notes to notes.ts

--- src/notes.ts
+++ src/notes.ts
@@ -8,13 +8,15 @@ console.log(circ(10))
 
 
 //when declaring types, use :
-// explicit types
+// explicit types // so we declare the variable and give it a type
 let character: string;
 let age: number;
 let isLoggedIn: boolean;
+let isLoggedOut: (boolean| string);
+// then when we assign that variable it can only be a certain type
 
 // explicit types arrays
-
+// set a variable to a specific array type - this says that anything in this array can only be a string
 let ninjas: string[];
 ninjas = ['yogshi', 'mario']
 
@@ -22,20 +24,18 @@ ninjas = ['yogshi', 'mario']
 // this allows us to store strings or numbers or booleans in this array
 let mixed: (string|number|boolean)[] = [];
 
+let somethingElse: (string|boolean)[] = [];
+
 // this is a single variable - the declaration of its type is string or number
 let uid: string|number;
 
 // explicit types - objects
 // this variable can only be an object
 let ninjaOne: object;
+// but you cant then declare variable inside of this object to be specific types but you can declare those variable types outside of the object
 
-// we can explicityly declare stuff instide of an object
-let nt: {
-    name: string,
-    age: number,
-    beltColor: string,
-    isCool: boolean
-};
+// we can explicityly declare stuff in: stide of an object
+let ninja: { name: string, age: number, beltColor: string, isCool: boolean };
 
 // type any means this can be any type
 let test: any = 25;
@@ -47,8 +47,8 @@ greet = () => {
     console.log("hello")
 }
 
-// use ? like a?: to declare optional value or set a default value for the variable after its type is declared
-// dont use optional value or default value together - alwasy put default optional at the end
+// use ? like a?: to declare optional value -- or you can set a default value for the variable after its type is declared
+// dont use optional value or default value together -- always put default optional at the end
 const add = (a: number, b: number, c: number | string = 10) => {
     console.log(a + b);
 }
@@ -105,7 +105,8 @@ logDeets = (ninja: {name: string, age: number}) => {
     console.log(`${ninja.name} is ${ninja.age} years old`);
 }
 
-// classes
+
+
 // class Invoice {
 //     // cannot be changed / it can still be accessed but it CANNOT ever be changed
 //     readonly client: string;
@@ -124,3 +125,4 @@ logDeets = (ninja: {name: string, age: number}) => {
 //         return `${this.client} owes #${this.amount} for ${this.details}`;
 //     }
 // }
+
